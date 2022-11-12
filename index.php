b83a1bb3750328b32a73319b5a3d16a3f5c3a91c
Serge HARDY <sergehardy@users.noreply.github.com>
2022-11-12 21:00:05 +0100

    WIP

--- index.php
+++ index.php
@@ -10,11 +10,25 @@ if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
 ?>
 <html>
 <head>
-<title>Communauté de pratiques environnementale</title>
+
+    <title>Communauté de pratiques environnementale</title>
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
 </head>
 <body>
 <h1>Hello Green Coop !</h1>
 <br />
-<a href="greencoopnet.slack.com" title="Slack">Slack</a>
+<a href="https://greencoopnet.slack.com/" title="Slack">Slack</a>
+<br />
+<a href="https://www.linkedin.com/company/greencoopnet/" title="Linkedin">Linkedin</a>
+<br />
+<p>
+    Email : contact at greencoop dot net
+</p>
+
 </body>
 </html>
